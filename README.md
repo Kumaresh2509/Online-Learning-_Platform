bf38f1aa7e88f0847a75ae9e8c868eb1620c1195
Kumaresh2509 <kumareshramya276@gmail.com>
2025-05-30 16:47:28 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Online learning Platform
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/kumaresh-N/pen/jEPbgPZ](https://codepen.io/kumaresh-N/pen/jEPbgPZ).
+
---
+++ script.js
@@ -0,0 +1,7 @@
+< div
+  className="min-h-screen bg-cover bg-center text-gray-800 p-4"
+  style={{
+    backgroundImage:
+      "url('https://images.unsplash.com/photo-1747134392591-0c748c9727b3?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDg2MDA2MDR8&ixlib=rb-4.1.0&q=85')",
+  }}
+>
